2c967268490eca7c7e73368328087ae1d10759d4
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-12-01 11:05:09 +0600

    updated skills

--- src/components/Home/Skills/Skills.js
+++ src/components/Home/Skills/Skills.js
@@ -26,20 +26,20 @@ const Skills = () => {
     }
 
     const webData = [
-        { name: "REACT", bgcolor: "#d63031", completed: 80 },
-        { name: "NODE", bgcolor: "#d63031", completed: 50 },
-        { name: "MONGO DB", bgcolor: "#d63031", completed: 65 },
+        { name: "REACT", bgcolor: "#d63031", completed: 88 },
+        { name: "NODE", bgcolor: "#d63031", completed: 70 },
+        { name: "MONGO DB", bgcolor: "#d63031", completed: 75 },
         { name: "EXPRESS JS", bgcolor: "#d63031", completed: 70 },
-        { name: "HTML", bgcolor: "#d63031", completed: 90 },
-        { name: "CSS", bgcolor: "#d63031", completed: 70 },
+        { name: "HTML", bgcolor: "#d63031", completed: 98 },
+        { name: "CSS", bgcolor: "#d63031", completed: 90 },
     ];
     const programmingData = [
         { name: "JavaScript", bgcolor: "#d63031", completed: 80 },
         { name: "PHP", bgcolor: "#d63031", completed: 40 }
     ];
     const toolsData = [
-        { name: "GIT", bgcolor: "#d63031", completed: 80 },
-        { name: "FIREBASE", bgcolor: "#d63031", completed: 70 },
+        { name: "GIT", bgcolor: "#d63031", completed: 88 },
+        { name: "FIREBASE", bgcolor: "#d63031", completed: 85 },
         { name: "PHOTOSHOP", bgcolor: "#d63031", completed: 55 }
     ];
 
@@ -50,9 +50,9 @@ const Skills = () => {
                     <h2 className="text-center">My skills</h2>
 
                     <Nav style={{ marginBottom: '28px', marginTop: "50px" }} className="align-items-center main-nav">
-                        <Nav.Link style={{backgroundColor: web ? '#d63031' : "#111A28"}} onClick={webShow} className="nav-item main-about skill-btn">Web</Nav.Link>
-                        <Nav.Link style={{backgroundColor: programming ? '#d63031' : "#111A28"}} onClick={programmingShow} className="nav-item main-about skill-btn">Programming</Nav.Link>
-                        <Nav.Link style={{backgroundColor: tools ? '#d63031' : "#111A28"}} onClick={toolsShow} className="nav-item main-about skill-btn">Tools</Nav.Link>
+                        <Nav.Link style={{backgroundColor: web ? '#d63031' : "#111A28", marginBottom:"5px"}} onClick={webShow} className="nav-item main-about skill-btn">Web</Nav.Link>
+                        <Nav.Link style={{backgroundColor: programming ? '#d63031' : "#111A28", marginBottom:"5px"}} onClick={programmingShow} className="nav-item main-about skill-btn">Programming</Nav.Link>
+                        <Nav.Link style={{backgroundColor: tools ? '#d63031' : "#111A28", marginBottom:"5px"}} onClick={toolsShow} className="nav-item main-about skill-btn">Tools</Nav.Link>
                     </Nav>
                     <Row>
                         {web && webData.map((item, idx) => (<ProgressBar key={idx} name={item.name} bgcolor={item.bgcolor} completed={item.completed}></ProgressBar>))}
